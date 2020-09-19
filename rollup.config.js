b3c75a2cad389f9d96a6eaf4d86b2ad661179044
Nutthapat Piasangka <n.devs260340@gmail.com>
2020-09-19 22:15:06 +0700

    update

---
+++ rollup.config.js
@@ -0,0 +1,22 @@
+import commonjs from "rollup-plugin-commonjs";
+
+export default [
+  {
+    input: "lib/cli-index.js",
+    output: {
+      file: "dist/cli-index.js",
+      format: "cjs"
+    },
+    external: ["shelljs", "yargs"]
+  },
+  {
+    input: "lib/index.js",
+    output: {
+      file: "dist/index.js",
+      format: "cjs"
+    },
+    plugins: [
+      commonjs()
+    ]
+  }
+];
